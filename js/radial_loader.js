0d6652f2165629ef9803d9f735c324c0def2110e
James Bosworth <james@bosworth.co>
2014-09-17 13:47:30 +1200

    Created LESS loop

--- js/radial_loader.js
+++ js/radial_loader.js
@@ -9,18 +9,11 @@ $(document).ready(function(){
   		var progress = new changePercentage();
 
   		var gui = new dat.GUI();
-  			gui.add(progress, 'percentage', 0, 180);
+  			gui.add(progress, 'percentage', 0, 100);
 
   		var updateProgress = function() {
-	  		var transform_styles = ['-webkit-transform', '-ms-transform', 'transform'];
 	  		var rotation = Math.floor(progress.percentage);
-			var fill_rotation = rotation;
-			var fix_rotation = rotation * 2;
-
-			for(i in transform_styles) {
-				$('.circle .fill, .circle .mask.full').css(transform_styles[i], 'rotate(' + fill_rotation + 'deg)');
-				$('.circle .fill.fix').css(transform_styles[i], 'rotate(' + fix_rotation + 'deg)');
-			}
+			$('.loader__progress').attr('data-progress', rotation);
   		};
   		setInterval( updateProgress, 500 );
 
